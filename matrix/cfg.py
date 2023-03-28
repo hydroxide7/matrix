20e895a3e672213d6c7b26963e6147b59e6694cb
hydroxide7 <126918374+hydroxide7@users.noreply.github.com>
2023-03-28 11:05:13 -0400

    Update cfg.py
    for 1.3

--- matrix/cfg.py
+++ matrix/cfg.py
@@ -1,11 +1,20 @@
 precision = 0.00000001
 output_precision = 0.0001
-input_value = None
+max_denominator = 100
+
 width_abs = 0
 width = 0
 height = 0
 pivot_loc = []
 free_var_loc = []
 determinant = 1
-max_denominator = 100
 max_power = 0
+
+def reset():
+    width_abs = 0
+    width = 0
+    height = 0
+    pivot_loc = []
+    free_var_loc = []
+    determinant = 1
+    max_power = 0    
